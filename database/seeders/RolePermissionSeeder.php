2e3dba8e6a70374dfc928b848a6dc73e8b04cbba
restuah <207006069@student.unsil.ac.id>
2024-05-06 10:04:25 +0700

    Baru Database

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $ownerRole = Role::create([
+            'name' => 'owner'
+        ]);
+
+        $buyerRole = Role::create([
+            'name' => 'buyer'
+        ]);
+
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
+
+        $user->assignRole($ownerRole);
+    }
+}
